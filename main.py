0a487f97a33006055024f0a8c0f339d722ca0461
Kieran Triggs <kieranjamestriggs@gmail.com>
2023-04-10 13:16:15 +0000

    - defined functions and variables - added main calculations

--- main.py
+++ main.py
@@ -46,23 +46,74 @@ number_of_flights_less = input("How many flights have you taken in the past year
 number_of_flights_more = input("How many flights have you taken in the past year (4 hours or more): ")
 
 recycle_newspaper = input("Do you recycle newspaper? (y/n): ")
+recycle_newspaper = recycle_newspaper.lower()
 
 recycle_aluminium = input("Do you recycle aluminium and tin? (y/n): ")
+recycle_aluminium = recycle_aluminium.lower()
 
-def electric():
-  monthly_electric = monthly_electric * 105
-  return monthly_electric
+trees = float(input("how many trees have you planted this year?: "))
 
-def gas(): 
-  monthly_gas = monthly_gas * 105
-  return monthly_gas
-
-def oil(): 
-  monthly_oil = monthly_oil * 113
-  return monthly_oil
-
-def yearly_mileage():
-  total_yearly_mileage = total_yearly_mileage * 0.79
-  return total_yearly_mileage
+carbon_total = 0
 
 
+def electric(monthly_electric, carbon_total):
+    monthly_electric = monthly_electric * 105
+    carbon_total = carbon_total + monthly_electric
+    return monthly_electric, carbon_total
+
+
+def gas(monthly_gas, carbon_total):
+    monthly_gas = monthly_gas * 105
+    carbon_total = carbon_total + monthly_gas
+    return monthly_gas, carbon_total
+
+
+def oil(monthly_oil, carbon_total):
+    monthly_oil = monthly_oil * 113
+    carbon_total = carbon_total + monthly_oil
+    return monthly_oil, carbon_total
+
+
+def yearly_mileage(total_yearly_mileage, carbon_total):
+    total_yearly_mileage = total_yearly_mileage * 0.79
+    carbon_total = carbon_total + total_yearly_mileage
+    return total_yearly_mileage, carbon_total
+
+
+def flights_less(number_of_flights_less, carbon_total):
+    number_of_flights_less = number_of_flights_less * 1100
+    carbon_total = carbon_total + number_of_flights_less
+    return number_of_flights_less, carbon_total
+
+
+def flights_more(number_of_flights_more, carbon_total):
+    number_of_flights_more = number_of_flights_more * 4400
+    carbon_total = carbon_total + number_of_flights_more
+    return number_of_flights_more, carbon_total
+
+
+def recycle_paper(recycle_newspaper, carbon_total):
+    if recycle_newspaper == "y":
+        recycle_newspaper = True
+        carbon_total = carbon_total + 0
+    elif recycle_newspaper == "n":
+        recycle_newspaper = False
+        carbon_total = carbon_total + 184
+    return recycle_newspaper, carbon_total
+
+
+def recycle_alu_tin(recycle_aluminium, carbon_total):
+    if recycle_aluminium == "y":
+        recycle_aluminium = True
+        carbon_total = carbon_total + 0
+    elif recycle_aluminium == "n":
+        recycle_aluminium = False
+        carbon_total = carbon_total + 166
+    return recycle_aluminium, carbon_total
+
+
+def trees_count(trees, carbon_total):
+    carbon_offset = 46.2971
+    carbon_offset = carbon_offset * trees
+    carbon_total = carbon_total - carbon_offset
+    return carbon_offset, carbon_total
