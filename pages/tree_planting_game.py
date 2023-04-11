3d85f4d6100f970fc5a78e232f5f6b77ca518a19
Kieran Triggs <kieranjamestriggs@gmail.com>
2023-04-11 19:15:51 +0000

    - bug fixes part 3

--- pages/tree_planting_game.py
+++ pages/tree_planting_game.py
@@ -4,7 +4,7 @@ st.image(file)
 
 st.header('Download our educational game')
 
-with open("game.txt", "rb") as file:
+with open("pages/game.txt", "rb") as file:
     btn = st.download_button(
             label="Download Game",
             data=file,
