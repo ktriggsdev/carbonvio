50078fbd0df8b326b98cb421a6523ce151dc2415
Kieran Triggs <kieranjamestriggs@gmail.com>
2023-04-11 18:53:18 +0000

    - changed name of file to carbon_footprint_calculator.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 file = 'img/carbonvio.png'
 st.image(file)
 
-st.header('Welcome to Carbonvio!')
+st.header('Welcome to Carbonvio!!')
 st.subheader('Utility usage:')
 yearly_electric = st.number_input('How many kwh of electricity do you use per year?: ', 0, 100000)
 yearly_electric = float(yearly_electric)
