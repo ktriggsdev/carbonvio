f9011cf984621a7e9cc5b6c85312f32e3bf9347b
Kieran Triggs <kieranjamestriggs@gmail.com>
2023-04-11 12:55:14 +0000

    - added text_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,8 @@ import numpy as np
 
 st.title('Carbonvio')
 st.header('Welcome to Carbonvio!')
-st.text_input('How many kwh of electricity do you use per year?: ')
+yearly_electric = st.text_input('How many kwh of electricity do you use per year?: ')
+yearly_natural_gas = st.text_input('How many therms of natural gas do you use per year?: ')
+yearly_propane_gas = st.text_input('how many gallons of propane gas do you use per year?: ')
+yearly_oil = st.text_input('how many gallons of oil do you use per year for heating purposes?: ')
+total_yearly_mileage = st.text_input('how many miles have you done in your vehicle this year?: ')
