a069f0c9808d92e21ef3813f45f5137ead7eea98
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-04-15 15:45:05 +0100

    Update carbon_footprint_calculator.py

--- carbon_footprint_calculator.py
+++ carbon_footprint_calculator.py
@@ -2,35 +2,65 @@ import streamlit as st
 
 file = 'img/carbonvio.png'
 st.image(file)
-
 st.title('Welcome to Carbonvio!')
-st.subheader('Utility usage:')
-yearly_electric = st.number_input('How many KWh of electricity do you use per year?: ', 0, 100000)
-yearly_electric = float(yearly_electric)
-yearly_natural_gas = st.number_input('How many cubic meters of natural gas do you use per year?: ', 0, 100000)
-yearly_natural_gas = float(yearly_natural_gas)
-yearly_propane_gas = st.number_input('how many cubic meters of propane gas do you use per year?: ', 0, 100000)
-yearly_propane_gas = float(yearly_propane_gas)
-yearly_oil = st.number_input('how many litres of oil do you use per year for heating purposes?: ', 0, 100000)
-yearly_oil = float(yearly_oil)
-st.subheader('Transport:')
-total_yearly_mileage = st.number_input('how many miles have you done in your vehicle this year?: ', 1, 100000)
-total_yearly_mileage = float(total_yearly_mileage)
-total_yearly_gallons = total_yearly_mileage / 25
-total_yearly_gallons = float(total_yearly_gallons)
-number_of_flights_less = st.number_input('How many flights have you taken this year that are 4 hours or less?: ', 0, 50)
-number_of_flights_less = float(number_of_flights_less)
-number_of_flights_more = st.number_input('How many flights have you taken this year that are 4 hours or more?: ', 0, 50)
-number_of_flights_more = float(number_of_flights_more)
-st.subheader('Recycling')
-recycle_newspaper = st.selectbox('Do you recycle newspaper? (y/n): ', ['Yes', 'No'])
-recycle_aluminium = st.selectbox('Do you recycle aluminium and tin? (y/n): ', ['Yes', 'No'])
+metric_imperial = st.selectbox('Are you Metric or Imperial(US) (Metric/Imperial): ', ['Metric', 'Imperial'])
+if metric_imperial == 'Metric':
+    st.subheader('Utility usage:')
+    yearly_electric = st.number_input('How many KWh of electricity do you use per year?: ', 0, 100000)
+    yearly_electric = float(yearly_electric)
+    yearly_natural_gas = st.number_input('How many cubic meters of natural gas do you use per year?: ', 0, 100000)
+    yearly_natural_gas = float(yearly_natural_gas)
+    yearly_propane_gas = st.number_input('how many cubic meters of propane gas do you use per year?: ', 0, 100000)
+    yearly_propane_gas = float(yearly_propane_gas)
+    yearly_oil = st.number_input('how many litres of oil do you use per year for heating purposes?: ', 0, 100000)
+    yearly_oil = float(yearly_oil)
+    st.subheader('Transport:')
+    total_yearly_mileage = st.number_input('how many miles have you done in your vehicle this year?: ', 1, 100000)
+    total_yearly_mileage = float(total_yearly_mileage)
+    total_yearly_gallons = total_yearly_mileage / 25
+    total_yearly_gallons = float(total_yearly_gallons)
+    number_of_flights_less = st.number_input('How many flights have you taken this year that are 4 hours or less?: ', 0, 50)
+    number_of_flights_less = float(number_of_flights_less)
+    number_of_flights_more = st.number_input('How many flights have you taken this year that are 4 hours or more?: ', 0, 50)
+    number_of_flights_more = float(number_of_flights_more)
+    st.subheader('Recycling')
+    recycle_newspaper = st.selectbox('Do you recycle newspaper? (y/n): ', ['Yes', 'No'])
+    recycle_aluminium = st.selectbox('Do you recycle aluminium and tin? (y/n): ', ['Yes', 'No'])
 
-trees = st.number_input("how many trees have you planted this year?: ", 0, 1000)
-trees = float(trees)
-carbon_offset = 46.2971
+    trees = st.number_input("how many trees have you planted this year?: ", 0, 1000)
+    trees = float(trees)
+    carbon_offset = 46.2971
 
-carbon_total = 0.0
+    carbon_total = 0.0
+elif metric_imperial == 'Imperial':
+    
+    st.subheader('Utility usage:')
+    yearly_electric = st.number_input('How many Megajoules of electricity do you use per year?: ', 0, 100000)
+    yearly_electric = float(yearly_electric)
+    yearly_natural_gas = st.number_input('How many gallons of natural gas do you use per year?: ', 0, 100000)
+    yearly_natural_gas = float(yearly_natural_gas)
+    yearly_propane_gas = st.number_input('how many gallons of propane gas do you use per year?: ', 0, 100000)
+    yearly_propane_gas = float(yearly_propane_gas)
+    yearly_oil = st.number_input('how many gallons of oil do you use per year for heating purposes?: ', 0, 100000)
+    yearly_oil = float(yearly_oil)
+    st.subheader('Transport:')
+    total_yearly_mileage = st.number_input('how many miles have you done in your vehicle this year?: ', 1, 100000)
+    total_yearly_mileage = float(total_yearly_mileage)
+    total_yearly_gallons = total_yearly_mileage / 25
+    total_yearly_gallons = float(total_yearly_gallons)
+    number_of_flights_less = st.number_input('How many flights have you taken this year that are 4 hours or less?: ', 0, 50)
+    number_of_flights_less = float(number_of_flights_less)
+    number_of_flights_more = st.number_input('How many flights have you taken this year that are 4 hours or more?: ', 0, 50)
+    number_of_flights_more = float(number_of_flights_more)
+    st.subheader('Recycling')
+    recycle_newspaper = st.selectbox('Do you recycle newspaper? (y/n): ', ['Yes', 'No'])
+    recycle_aluminium = st.selectbox('Do you recycle aluminium and tin? (y/n): ', ['Yes', 'No'])
+
+    trees = st.number_input("how many trees have you planted this year?: ", 0, 1000)
+    trees = float(trees)
+    carbon_offset = 46.2971
+
+    carbon_total = 0.0
 
 yearly_electric = yearly_electric * 0.994
 carbon_total = carbon_total + yearly_electric
