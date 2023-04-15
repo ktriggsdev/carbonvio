c5df57f4cddafa02f8fa4d6ff21bf249e916d96f
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-04-15 18:35:06 +0100

    Update carbon_footprint_calculator.py

--- carbon_footprint_calculator.py
+++ carbon_footprint_calculator.py
@@ -132,16 +132,18 @@ if yearly_electric > 2900:
         'Install a smart meter'
     ]
 
-    electricity_bad = '<h3 style="font-family:monospace; color: #ef233c;">' \
-                      'Tips to cut back on electricity usage:</h3>'
-    st.markdown(electricity_bad, unsafe_allow_html=True)
+#     electricity_bad = '<h3 style="font-family:monospace; color: #ef233c;">' \
+#                       'Tips to cut back on electricity usage:</h3>'
+#     st.markdown(electricity_bad, unsafe_allow_html=True)
+    electricity_bad = st.warning("Tips to cut back on electricity usage:")
 
-    electricity_sub_bad = '<p style="font-family:monospace; color: #ef233c; font-size: 14px;">' \
-                          'You are using too much electricity! here are some ways to cut back on your usage: </p>'
-    st.markdown(electricity_sub_bad, unsafe_allow_html=True)
-
+#     electricity_sub_bad = '<p style="font-family:monospace; color: #ef233c; font-size: 14px;">' \
+#                           'You are using too much electricity! here are some ways to cut back on your usage: </p>'
+#     st.markdown(electricity_sub_bad, unsafe_allow_html=True)
+    electricity_sub_bad = st.error('You are using too much electricity! here are some ways to cut back on your usage:')
+   
     for item in electricity_tips:
-        electricity_message = st.code(item)
+        electricity_message = st.error(item)
 else:
     electricity_good = '<h3 style="font-family:monospace; color: #01FD7A;">' \
                          'Electricity: Good </h3>'
