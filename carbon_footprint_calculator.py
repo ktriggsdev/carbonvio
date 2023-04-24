8fe096184beac9cc8397fc877018b1bd694add47
H34TW4V3 <126419345+H34TW4V3@users.noreply.github.com>
2023-04-24 22:48:31 +0100

    Change 1: Added new login screen (I think) Change 2: Fixed an Indent issue caused by my IDEs dodgy formatting.

--- carbon_footprint_calculator.py
+++ carbon_footprint_calculator.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import hashlib
 
 file = 'img/carbonvio.png'
 
@@ -15,6 +17,59 @@ st.image(file)
 # sets a title for the page
 st.title('Welcome to Carbonvio!')
 
+# Define a filename to store the user credentials
+filename = 'credentials.csv'
+
+# Load the dataframe from the file if it exists, or create a new one
+try:
+    df = pd.read_csv(filename)
+except FileNotFoundError:
+    df = pd.DataFrame({
+        'username': ['admin'],
+        'password': ['5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', # hash of 'password'
+                     'e38ad214943daad1d64c102faec29de4afe9da3d', # hash of 'streamlit'
+                     '5e52fee47e6f95708eab44de4c0f3dab9d21b9db'] # hash of 'python'
+    })
+
+# Define a function to hash passwords
+def hash_password(password):
+    return hashlib.sha1(password.encode('utf-8')).hexdigest()
+
+# Define a function to check if the username and password are valid
+def check_credentials(username, password):
+    password_hash = hash_password(password)
+    return (username in df['username'].values) and (password_hash == df[df['username'] == username]['password'].iloc[0])
+
+# Define a function to register a new account
+def register_account(username, password):
+    password_hash = hash_password(password)
+    # Check if the username is already taken
+    if username in df['username'].values:
+        return False
+    else:
+        # Append the new credentials to the dataframe
+        df.loc[len(df)] = [username, password_hash]
+        # Save the dataframe to the file
+        df.to_csv(filename, index=False)
+        return True
+
+# Create a title and a sidebar
+st.title('Login System')
+sidebar = st.sidebar
+
+# Ask the user to choose between login or register in the sidebar
+mode = sidebar.radio('Choose mode', ['Login', 'Register'])
+
+# If the user chooses login, ask them to enter username and password in the sidebar
+if mode == 'Login':
+    username = sidebar.text_input('Username')
+    password = sidebar.text_input('Password', type='password')
+
+    # If the user clicks the login button, check the credentials and display a message
+    if sidebar.button('Login'):
+        if check_credentials(username, password):
+            st.success('Welcome back {}'.format(username))
+            # You can add your app logic here
 # user chooses either metric or imperial, the results differ for each option
 metric_imperial = st.selectbox('Are you Metric or Imperial(US) (Metric/Imperial): ', ['Metric', 'Imperial'])
 if metric_imperial == 'Metric':
@@ -323,3 +378,25 @@ else:
 # carbon footprint result is displayed
 st.subheader('Your Carbon Footprint:')
 carbon_total = st.text(f'Your Carbon Footprint is {carbon_total} tonnes of CO2')
+
+            # Add a button to log out
+if st.button('Log out'):
+                st.info('You have been successfuly logged out! Goodbye.')
+                # Clear the username and password inputs
+                sidebar.empty()
+                # Reload the page
+                st.experimental_rerun()
+else:
+            st.error('Sorry, that isnt a valid username or password. Please try again')
+
+# If the user chooses register, ask them to enter a new username and password in the sidebar
+elsif  .mode == 'Register'
+new_username = sidebar.text_input('Create a username')
+new_password = sidebar.text_input('Create a new password', type='password')
+
+    # If the user clicks the register button, register a new account and display a message
+if sidebar.button('Register'):
+        if register_account(new_username, new_password):
+            st.success('Welcome to Carbonvio!. You can now login with your new credentials.')
+        else:
+            st.error('Sorry, that username is already taken. Please choose a different one.')
